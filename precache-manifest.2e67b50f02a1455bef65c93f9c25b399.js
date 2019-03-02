734ccb0a42f95d892e3109c7f3a78bafdd5f3967
Emir Mekić <emir.mekic@live.com>
2019-03-02 12:33:27 +0100

    Updates

---
+++ precache-manifest.2e67b50f02a1455bef65c93f9c25b399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ed269fdb52b4596064a",
+    "url": "/caffe-pos-new/static/js/runtime~main.7ed269fd.js"
+  },
+  {
+    "revision": "1c2802c66a1b3178bbaf",
+    "url": "/caffe-pos-new/static/js/main.1c2802c6.chunk.js"
+  },
+  {
+    "revision": "b5b3d63aa76378c20cdd",
+    "url": "/caffe-pos-new/static/js/2.b5b3d63a.chunk.js"
+  },
+  {
+    "revision": "1c2802c66a1b3178bbaf",
+    "url": "/caffe-pos-new/static/css/main.846f4dbd.chunk.css"
+  },
+  {
+    "revision": "b5b3d63aa76378c20cdd",
+    "url": "/caffe-pos-new/static/css/2.2e05c877.chunk.css"
+  },
+  {
+    "revision": "6a276d5017e6915a896ac4a1641665bf",
+    "url": "/caffe-pos-new/index.html"
+  }
+];
